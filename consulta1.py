18475f795b8b99e6e5266e0f6adccd925a88ecef
raul8005 <rimedina@utpl.edu.ec>
2025-05-27 08:48:47 -0500

    Consulta1

---
+++ consulta1.py
@@ -0,0 +1,31 @@
+# 1. Obtener las entregas de todos los estudiantes que pertenecen al departamento de Arte. 
+# En función de la entrega, presentar, nombre del tarea, nombre del estudiante, calificación, 
+# nombre de instructor y nombre del departamento
+
+from sqlalchemy.orm import sessionmaker
+#from clases import engine, Departamento, Instructor, Curso, Estudiante, Inscripcion, Tarea, Entrega
+from clases import *
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+entregas_arte = (
+    session.query(Entrega)
+    .join(Entrega.tarea)
+    .join(Tarea.curso)
+    .join(Curso.departamento)
+    .join(Curso.instructor)
+#    .join(Entrega.estudiante)
+    .filter(Departamento.nombre == 'Arte').all()
+)
+
+print("Entregas de los estudiantes en el Departamento de Arte")
+
+for entrega in entregas_arte:
+	print("-------------------------------------------")
+	print(f"Nombre de la tarea: {entrega.tarea.titulo}")
+	print(f"Nombre del estudiante: {entrega.estudiante.nombre}")
+	print(f"Calificacion: {entrega.calificacion}")
+	print(f"Nombre del Instructor: {entrega.tarea.curso.instructor.nombre}")
+	print(f"Nombre del Departamento: {entrega.tarea.curso.departamento.nombre}")
+	print("-------------------------------------------")
